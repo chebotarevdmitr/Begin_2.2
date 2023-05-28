469d651aa32dd922cfdc905e92376778d857dcc9
DIM <4ebotarov_dim@inbox.ru>
2023-05-28 21:24:46 +0300

    double

--- Begin_2.2.cpp
+++ Begin_2.2.cpp
@@ -3,13 +3,16 @@ using namespace std;
 
 int main() {
 //переменные типы данных
-// int - целочисленный тип ,year_of_birth - год рождения	
+// int - целочисленный тип      4 байта    ,year_of_birth - год рождения	
    int year_of_birth = 1978;	
-//  char - символьный тип ,	gender - пол 
+//  char - символьный тип ,     1 байт     gender - пол 
 	char gender ="f";
-// bool - лгический тип ,is_older_than_18 -старше 18 , true - правда ,folse - ложь	
+// bool - лгический тип ,       4 байта   is_older_than_18 -старше 18 , true - правда ,folse - ложь	
 	bool is_older_than_18 = true ;
-	
+// float - десятичный тип (с плавающей точкой )  4 байта    ,average_grad - средняя оценка	
+    float average_grad = 4.5; 
+// double - двойной десятичный тип  8 байт     balance -баланс
+   double balance =292929292877;	
 	
 	system("pause>0");
 }
