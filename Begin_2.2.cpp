5c1ceb3fbcaa373195b47fd8c12adcb1faf89a8d
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-05-28 21:42:47 +0300

    int_byt add min

--- Begin_2.2.cpp
+++ Begin_2.2.cpp
@@ -6,13 +6,16 @@ int main() {
 // int - целочисленный тип      4 байта    ,year_of_birth - год рождения	
    int year_of_birth = 1978;	
 //  char - символьный тип ,     1 байт     gender - пол 
-	char gender ="f";
+	char gender = 'f';
 // bool - лгический тип ,       4 байта   is_older_than_18 -старше 18 , true - правда ,folse - ложь	
 	bool is_older_than_18 = true ;
 // float - десятичный тип (с плавающей точкой )  4 байта    ,average_grad - средняя оценка	
     float average_grad = 4.5; 
 // double - двойной десятичный тип  8 байт     balance -баланс
-   double balance =292929292877;	
+   double balance =292929292877;
+   cout<< "Size of int is "<<sizeof(int)<<"bytes \n";
+   cout<<"Int min value is"<< INT_MIN << endl;
+   	
 	
 	system("pause>0");
 }
