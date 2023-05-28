5d1bc2f103332205de7e367d82608ca9977e716c
DIM <4ebotarov_dim@inbox.ru>
2023-05-28 21:17:13 +0300

    tip dannih

--- Begin_2.2.cpp
+++ Begin_2.2.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int main() {
-	printf("Hello Begin 2.2");
+//переменные типы данных
+// int - целочисленный тип ,year_of_birth - год рождения	
+   int year_of_birth = 1978;	
+//  char - символьный тип ,	gender - пол 
+	char gender ="f";
+// bool - лгический тип ,is_older_than_18 -старше 18 , true - правда ,folse - ложь	
+	bool is_older_than_18 = true ;
 	
 	
 	system("pause>0");
