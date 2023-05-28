1857d32c91532ce8f42eae339823d83080710068
DIM <4ebotarov_dim@inbox.ru>
2023-05-28 22:49:20 +0300

    overflow

--- Begin_2.2.cpp
+++ Begin_2.2.cpp
@@ -18,8 +18,20 @@ int main() {
    // -1,-2,-3,....,-2147483648
    cout<<"Int min value is  "<< INT_MIN << endl;
    // 0,1,2,3,.....,2147483647
-   cout<<"Int max value is  "<< INT_MAX << endl; 	
-	
+   cout<<"Int max value is   "<< INT_MAX << endl; 	
+   
+   cout<<"Size of unsigned int is  "<< sizeof(unsigned int)<<"bytes\n";
+   cout<<"UInt value is  "<< UINT_MAX <<endl;
+   cout<<"Size of unsigned bool is  "<< sizeof (bool)<<"bytes\n";
+   cout<<"Size of unsigned char is  "<< sizeof (char)<<"bytes\n";
+   cout<<"Size of unsigned float is  "<< sizeof (float)<<"bytes\n";
+   cout<<"Size of unsigned double is  "<< sizeof (double)<<"bytes\n\n\n\n";
+   
+   cout <<"perepolnenie tipov dannyh "<<endl ;
+   int int_max = INT_MAX;
+   cout << int_max << endl;
+   cout << int_max +1;
+   	
 	system("pause>0");
 }
 	
